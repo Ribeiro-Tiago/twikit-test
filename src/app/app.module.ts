9d1c2dc8b2664126ffdb3fd17ae904406abe5379
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2021-05-10 17:01:32 +0200

    add toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/button/button.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ButtonComponent, ToolbarComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
