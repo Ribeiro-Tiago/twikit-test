292a1d1e70a1805cc565fc34219f08b17f443089
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2021-05-11 17:53:14 +0200

    add sort & refresh buttons to toolbar (actions missing)

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
+import { Timer } from 'src/app/typings';
+import { isTimerActive } from 'src/app/store/selectors/timer.selector';
+
+import { AppState } from 'src/app/typings/store';
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss']
+  styleUrls: ['./toolbar.component.scss'],
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent {
+  isTimerActive$: Observable<Timer>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private store: Store<AppState>) {
+    this.isTimerActive$ = this.store.pipe(select(isTimerActive));
   }
-
 }
