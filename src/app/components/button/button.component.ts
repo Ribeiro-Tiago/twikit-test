386f687cfbb16b5850eb7261bfe109197138168b
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2021-05-11 19:16:14 +0200

    add event & disable to button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,16 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent {
   @Input() label: string = '';
   @Input() color: string = '';
   @Input() icon: string = '';
+  @Input() disabled: boolean = false;
 
-  constructor() {}
+  @Output() onClick: EventEmitter<void> = new EventEmitter();
 
-  ngOnInit(): void {}
+  click() {
+    this.onClick.emit();
+  }
 }
