875b81f827ce28c2dc916c961697a01bb11774c5
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2021-05-11 19:08:09 +0200

    fix pipe

--- src/app/pipes/time/time.pipe.ts
+++ src/app/pipes/time/time.pipe.ts
@@ -2,20 +2,38 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'time' })
 export class TimePipe implements PipeTransform {
+  addPluralS(val: number, str: string) {
+    return val === 1 ? str : `${str}s`;
+  }
+
   transform(timestamp: number, ...args: unknown[]): unknown {
     if (timestamp === 0) {
-      return '0 secs';
+      return '0 seconds';
     }
 
-    const hours = Math.floor(timestamp / 3600);
+    const totalSeconds = timestamp / 1000;
 
-    timestamp %= 3600;
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = Math.floor((totalSeconds % 3600) % 60);
 
-    const minutes = Math.floor(timestamp / 60);
-    const seconds = timestamp % 60;
+    if (hours > 0) {
+      return `${hours} ${this.addPluralS(
+        hours,
+        'hour'
+      )} ${minutes} ${this.addPluralS(
+        minutes,
+        'minute'
+      )} ${seconds} ${this.addPluralS(seconds, 'second')}`;
+    }
 
-    console.log(hours, minutes, seconds);
+    if (minutes > 0) {
+      return `${minutes} ${this.addPluralS(
+        minutes,
+        'minute'
+      )} ${seconds} ${this.addPluralS(seconds, 'second')}`;
+    }
 
-    return timestamp;
+    return `${seconds} ${this.addPluralS(seconds, 'second')}`;
   }
 }
